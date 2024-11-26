325e00eadac6047277aae313de0750c611db9289
PurpleCat404 <bednyak43@mail.ru>
2024-11-26 21:11:05 +0500

    Refactoring. Nodes as class NodesHelper.

--- nodes_distribution.py
+++ nodes_distribution.py
@@ -4,36 +4,39 @@ import matplotlib.pyplot as plt
 
 
 VIRTUAL_NODES_COUNT = 10 # при увеличении в теории должно быть более равномерное распред
+
 def hash_function(key):
-    return int(hashlib.sha256(key.encode()).hexdigest(), 16) % (2**32)
+    return int(hashlib.sha256(key.encode()).hexdigest(), 16) % (2 ** 32)
 
-def initialize_tokens(nodes_keys = ["Node1", "Node2", "Node3"]):
-    tokens = {}  # Ключ: Виртуальное имя узла (несколько имён физического узла) Значение: имя физического узла
-    for node_key in nodes_keys:
-        for i in range(VIRTUAL_NODES_COUNT):
-            tokens[hash_function(f"{node_key}-{i}")] = node_key
+class NodesHelper:
 
-    sorted_virtual_nodes = sorted(tokens.keys())
-    return tokens, sorted_virtual_nodes
+    def __init__(self, nodes_keys = ["Node1", "Node2", "Node3"]):
+        self.tokens = {}  # Ключ: Виртуальное имя узла (несколько имён физического узла) Значение: имя физического узла
 
-def find_node_with_virtual_nodes(key, tokens, sorted_virtual_nodes):
-    key_hash = hash_function(key)
-    founded_node = bisect_left(sorted_virtual_nodes, key_hash)
+        for node_key in nodes_keys:
+            for i in range(VIRTUAL_NODES_COUNT):
+                self.tokens[hash_function(f"{node_key}-{i}")] = node_key
 
-    return tokens[sorted_virtual_nodes[0]] \
-        if founded_node == len(sorted_virtual_nodes) \
-        else tokens[sorted_virtual_nodes[founded_node]]
+        self.sorted_virtual_nodes = sorted(self.tokens.keys())
 
+    def find_node_for_key(self, key):
+        key_hash = hash_function(key)
+        founded_node = bisect_left(self.sorted_virtual_nodes, key_hash)
+
+        return self.tokens[self.sorted_virtual_nodes[0]] \
+            if founded_node == len(self.sorted_virtual_nodes) \
+            else self.tokens[self.sorted_virtual_nodes[founded_node]]
+
+#TESTING
 if __name__ == "__main__":
     nodes = [f"192.168.1.{i}" for i in range(1, 6)]
+    node_helper = NodesHelper(nodes)
     quantity_keys_in_node = {node: 0 for node in nodes}
-
-    tokens, sorted_virtual_nodes = initialize_tokens(nodes)
-
     num_keys = 10_000
+
     for i in range(1, num_keys + 1):
         smth_key = f"key-{i}"
-        node = find_node_with_virtual_nodes(smth_key, tokens, sorted_virtual_nodes)
+        node = node_helper.find_node_for_key(smth_key)
         quantity_keys_in_node[node] += 1
 
     for node, count in quantity_keys_in_node.items():
